2cf5d84903384c2acc1217488e1b7dc35ec0f6d6
Maciej Mituta <maciejmituta@gmail.com>
2023-10-25 19:26:05 +0200

    Add missing DataJpaTest class

---
+++ src/test/java/com/example/todo/items/repository/ItemRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.todo.items.repository;
+
+import com.example.todo.CurrentDateTimeProvider;
+import com.example.todo.items.model.Item;
+import com.example.todo.items.model.Status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.OffsetDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@DataJpaTest
+class ItemRepositoryTest
+{
+    @MockBean
+    private CurrentDateTimeProvider currentDateTimeProvider;
+    @Autowired
+    private ItemRepository itemRepository;
+
+
+    @Test
+    void shouldSetCreatedDateTimeWhenPersistingAnItem(){
+        when(this.currentDateTimeProvider.now()).thenReturn(OffsetDateTime.MIN);
+
+        Item saved = this.itemRepository.save(new Item(null, "desc", OffsetDateTime.MAX, null, null, Status.NOT_DONE));
+
+        assertThat(saved.getCreated()).isEqualTo(OffsetDateTime.MIN);
+    }
+}
